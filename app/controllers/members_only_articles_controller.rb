dc8e0660ed1593d131b197e9a025935047726228
Darrian Bagley <dbagle14@gmail.com>
2022-07-30 10:59:07 -0400

    Completed assignment

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -2,13 +2,21 @@ class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    if session[:user_id]
+      articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer
+    else
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if session[:user_id]
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
   end
 
   private
@@ -16,5 +24,4 @@ class MembersOnlyArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
 end
